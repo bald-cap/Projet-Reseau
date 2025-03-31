075143afbd24a92bf5fc71ff824fcd16693facf0
ElsyLauriaMpundu <elsy9059@gmail.com>
2025-03-31 16:40:13 +0200

    Gestion d'historique , abstraction du code

--- client_chat.c
+++ client_chat.c
@@ -8,10 +8,18 @@
 #include <signal.h>
 
 #define SERVER_IP "127.0.0.1"
-#define PORT 8080
+#define PORT 8082
 #define BUFFER_SIZE 1024
+#define SI struct sockaddr_in
+#define SA struct sockaddr
 
 int sock;
+char username[50];
+
+void exit_with_error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
 
 void handle_exit(int sig) {
     printf("\nDéconnexion en cours...\n");
@@ -37,40 +45,38 @@ void *receive_messages(void *socket_desc) {
 }
 
 int main() {
-    struct sockaddr_in server_address;
+    SI server_address;
     char message[BUFFER_SIZE];
+    char full_message[BUFFER_SIZE + 50];
     pthread_t recv_thread;
 
-    // Gestion du signal pour une fermeture propre
     signal(SIGINT, handle_exit);
 
-    // Création du socket
+    printf("Entrez votre nom: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = 0;
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Erreur lors de la création du socket");
-        exit(EXIT_FAILURE);
+        exit_with_error("Erreur lors de la création du socket");
     }
 
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
     if (inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr) <= 0) {
-        perror("Adresse invalide");
-        exit(EXIT_FAILURE);
+        exit_with_error("Adresse invalide");
     }
 
-    // Connexion au serveur
-    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("Connexion échouée");
-        exit(EXIT_FAILURE);
+    if (connect(sock, (SA *)&server_address, sizeof(server_address)) < 0) {
+        exit_with_error("Connexion échouée");
     }
     printf("Connecté au serveur. Vous pouvez commencer à chatter !\n");
 
-    // Lancer un thread pour recevoir les messages
     pthread_create(&recv_thread, NULL, receive_messages, (void *)&sock);
 
-    // Lire les messages de l'utilisateur et les envoyer
     while (1) {
         fgets(message, BUFFER_SIZE, stdin);
-        send(sock, message, strlen(message), 0);
+        snprintf(full_message, sizeof(full_message), "%s: %s", username, message);
+        send(sock, full_message, strlen(full_message), 0);
     }
 
     return 0;
--- serveur_chat.c
+++ serveur_chat.c
@@ -8,125 +8,159 @@
 #include <errno.h>
 #include <signal.h>
 
-#define PORT 8080
+#define PORT 8082
 #define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
 
-int server_fd;
-int client_sockets[MAX_CLIENTS];
+typedef struct {
+    int socket;
+    char username[50];
+} Client;
 
-void handle_sigint(int sig) {
+int server_fd;
+Client clients[MAX_CLIENTS];
+
+void exit_with_error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void close_all_sockets() {
     printf("\nArrêt du serveur...\n");
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_sockets[i] != 0) {
-            close(client_sockets[i]);
+        if (clients[i].socket != 0) {
+            close(clients[i].socket);
         }
     }
     close(server_fd);
     printf("Serveur arrêté proprement.\n");
+}
+
+void handle_sigint(int sig) {
+    close_all_sockets();
     exit(0);
 }
 
-int main() {
-    int new_socket, max_sd, activity, sd, valread;
+void save_message_to_file(const char *message) {
+    FILE *file = fopen("history.txt", "a");
+    if (file) {
+        fprintf(file, "%s\n", message);
+        fclose(file);
+    } else {
+        perror("Erreur d'ouverture du fichier");
+    }
+}
+
+void broadcast_message(const char *message, int sender_socket) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].socket != 0 && clients[i].socket != sender_socket) {
+            send(clients[i].socket, message, strlen(message), 0);
+        }
+    }
+}
+
+void handle_new_connection() {
     struct sockaddr_in address;
     socklen_t addrlen = sizeof(address);
+    int new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+
+    if (new_socket < 0) {
+        perror("Erreur lors de l'acceptation");
+        return;
+    }
+
+    printf("Nouvelle connexion acceptée, socket: %d\n", new_socket);
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].socket == 0) {
+            clients[i].socket = new_socket;
+            
+            return;
+        }
+    }
+
+    // Si le serveur est plein
+    char *msg = "Serveur plein, connexion refusée.\n";
+    send(new_socket, msg, strlen(msg), 0);
+    close(new_socket);
+}
+
+void handle_client_message(int client_index) {
+    int sock = clients[client_index].socket;
     char buffer[BUFFER_SIZE];
+    int valread = read(sock, buffer, BUFFER_SIZE);
+
+    if (valread == 0) {
+        printf("Client déconnecté, socket: %d\n", sock);
+        close(sock);
+        clients[client_index].socket = 0;
+    } else {
+        buffer[valread] = '\0';
+        printf("Message reçu de %s: %s", clients[client_index].username, buffer);
+        save_message_to_file(buffer);
+        broadcast_message(buffer, sock);
+    }
+}
+
+int main() {
+    struct sockaddr_in address;
     fd_set readfds;
-    int opt = 1;
+    int opt = 1, max_sd, activity;
 
     signal(SIGINT, handle_sigint);
 
-    // Initialisation des sockets clients
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        client_sockets[i] = 0;
+        clients[i].socket = 0;
     }
 
-    // Création du socket serveur
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Erreur lors de la création du socket");
-        exit(EXIT_FAILURE);
+        exit_with_error("Erreur lors de la création du socket");
     }
 
-    // Réutilisation de l'adresse et du port immédiatement après fermeture
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+        exit_with_error("setsockopt échoué");
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Liaison du socket
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Échec de la liaison");
-        exit(EXIT_FAILURE);
+        exit_with_error("Echec de la liaison");
     }
 
-    // Écoute des connexions entrantes
     if (listen(server_fd, MAX_CLIENTS) < 0) {
-        perror("Échec de l'écoute");
-        exit(EXIT_FAILURE);
+        exit_with_error("Echec de l'écoute");
     }
 
     printf("Serveur en écoute sur le port %d...\n", PORT);
 
     while (1) {
-        // Réinitialisation du set de descripteurs
         FD_ZERO(&readfds);
         FD_SET(server_fd, &readfds);
         max_sd = server_fd;
 
         for (int i = 0; i < MAX_CLIENTS; i++) {
-            sd = client_sockets[i];
-            if (sd > 0) FD_SET(sd, &readfds);
-            if (sd > max_sd) max_sd = sd;
+            if (clients[i].socket > 0) {
+                FD_SET(clients[i].socket, &readfds);
+            }
+            if (clients[i].socket > max_sd) {
+                max_sd = clients[i].socket;
+            }
         }
 
-        // Attente d'activité sur un des sockets
         activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-        if ((activity < 0) && (errno != EINTR)) {
+        if (activity < 0 && errno != EINTR) {
             perror("Erreur de select");
         }
 
-        // Nouvelle connexion entrante
         if (FD_ISSET(server_fd, &readfds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
-                perror("Erreur lors de l'acceptation");
-                exit(EXIT_FAILURE);
-            }
-            printf("Nouvelle connexion acceptée, socket: %d\n", new_socket);
-
-            // Ajout du nouveau client à la liste
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] == 0) {
-                    client_sockets[i] = new_socket;
-                    break;
-                }
-            }
+            handle_new_connection();
         }
 
-        // Vérification des clients existants
         for (int i = 0; i < MAX_CLIENTS; i++) {
-            sd = client_sockets[i];
-            if (FD_ISSET(sd, &readfds)) {
-                valread = read(sd, buffer, BUFFER_SIZE);
-                if (valread == 0) { // Déconnexion
-                    printf("Client déconnecté, socket: %d\n", sd);
-                    close(sd);
-                    client_sockets[i] = 0;
-                } else {
-                    buffer[valread] = '\0';
-                    printf("Message reçu de socket %d: %s", sd, buffer);
-
-                    // Diffuser le message à tous les clients
-                    for (int j = 0; j < MAX_CLIENTS; j++) {
-                        if (client_sockets[j] != 0 && client_sockets[j] != sd) {
-                            send(client_sockets[j], buffer, strlen(buffer), 0);
-                        }
-                    }
-                }
+            if (clients[i].socket > 0 && FD_ISSET(clients[i].socket, &readfds)) {
+                handle_client_message(i);
             }
         }
     }
